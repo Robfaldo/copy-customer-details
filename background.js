925a8ba8d5f819693461a79f49907cb6f988bb8b
Robert Faldo <robert.faldo@simplybusiness.co.uk>
2018-10-23 20:58:37 +0100

    only works on url path that includes backoffice/rfqs/

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {pathContains: 'backoffice/rfqs/'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
