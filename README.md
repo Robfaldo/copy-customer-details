3381a40cc905cb4506798171765e99e3ceb4b2ed
Robert Faldo <robert.faldo@simplybusiness.co.uk>
2018-10-23 23:04:46 +0100

    Pressing button console logs the actual document of current tab

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ TODO:
 - Change the url to simplybusiness
 - Clicking button grabs the elements and puts them in storage
 - ability to paste them somehow
+
+
+https://stackoverflow.com/questions/19641042/undefined-response-from-content-script-in-chrome-extension/19644584#19644584
+
+
+This one explains how I get the document from the current tab
+https://code.i-harness.com/en/q/12d7bcc
--- popup.js
+++ popup.js
@@ -1,19 +1,21 @@
 let copyDetails = document.getElementById('copyDetails');
 
-chrome.runtime.onMessage.addListener(
-   (request) => {
-     chrome.extension.getBackgroundPage().console.log(request);
-});
-
-
 copyDetails.onclick = function(element) {
-  var firstName = document.getElementById('customer_first_name');
-  chrome.extension.getBackgroundPage().console.log(document);
-  chrome.extension.getBackgroundPage().console.log("Clicking button prints console log to background page");
+  chrome.extension.getBackgroundPage().console.log("HelloThere");
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: "chrome.runtime.sendMessage({content: document})"});
+  function modifyDOM() {
+      //You can play with your DOM here or check URL against your regex
+      console.log('Tab script:');
+      console.log(document.body);
+      return document.body.innerHTML;
+  }
+
+  //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+  chrome.tabs.executeScript({
+      code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+  }, (results) => {
+      //Here we have just the innerHTML and not DOM structure
+      console.log('Popup script:')
+      console.log(results[0]);
   });
 };
