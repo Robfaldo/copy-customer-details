b2ac065f6bb9e304dc4c69a992b3fa9a4193415b
Robert Faldo <robert.faldo@simplybusiness.co.uk>
2018-10-23 22:26:32 +0100

    clicking button sends message with document (but not DOM)

--- popup.js
+++ popup.js
@@ -1,5 +1,19 @@
 let copyDetails = document.getElementById('copyDetails');
 
+chrome.runtime.onMessage.addListener(
+   (request) => {
+     chrome.extension.getBackgroundPage().console.log(request);
+});
+
+
 copyDetails.onclick = function(element) {
+  var firstName = document.getElementById('customer_first_name');
+  chrome.extension.getBackgroundPage().console.log(document);
   chrome.extension.getBackgroundPage().console.log("Clicking button prints console log to background page");
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: "chrome.runtime.sendMessage({content: document})"});
+  });
 };
